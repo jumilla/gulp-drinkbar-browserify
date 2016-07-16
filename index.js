4117dc45857dc61b530778b1a4061301e27f514f
Fumio Furukawa <fumio@jumilla.me>
2016-07-16 22:31:06 +0900

    First version.

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var stream = require('browserify');
+
+stream.babelify = require('babelify');
+
+module.exports = stream;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+const stream = require('browserify')
+
+stream.babelify = require('babelify')
+
+module.exports = stream
